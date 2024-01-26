28aa3b92d417c6a7bbf0cf2ee1201c5bc2ff12a1
WangshuXC <1294673511@qq.com>
2024-01-27 00:33:35 +0800

    音乐页面除了歌词显示基本完成

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -1,4 +1,8 @@
 <template>
+    <audio ref="audioPlayer" :src="audioSrc" @timeupdate="updateProgress"></audio>
+    <v-snackbar v-model="showSnackbar" :timeout="1000" rounded="pill" color="#fff" variant="tonal">
+        按下esc退出全屏
+    </v-snackbar>
     <div class="music-view">
         <a href="/" class="returnBtn">
             <button class="return"><font-awesome-icon :icon="['fas', 'angle-left']" /></button>
@@ -12,7 +16,7 @@
                     alt="专辑图片">
                 <div class="music-info">
                     <span class="title">{{ title }}</span>
-                    <span class="author">歌手</span>
+                    <span class="author">{{ author }}</span>
                 </div>
             </div>
             <div class="music-bar-switch">
@@ -26,33 +30,70 @@
                     <button @click="mode_toggle" class="modeBtn"><font-awesome-icon :icon="['fas', 'shuffle']" /></button>
                     <button @click="volume_toggle"><font-awesome-icon
                             :icon="isXmark ? ['fas', 'volume-xmark'] : ['fas', 'volume-high']" /></button>
-
                 </div>
-                <v-slider class="slider-bar" v-model="volume" color="white" hide-details="true" :thumb-size="15"></v-slider>
+                <v-slider v-model="volume" color="white" hide-details="true" @update:model-value="setVolume"
+                    :thumb-size="15"></v-slider>
             </div>
             <button @click="max" class="maxBtn"><font-awesome-icon :icon="['fas', 'chevron-up']" /></button>
         </div>
-
+        <div class="music-play-bg"></div>
         <div class="music-play">
+            <div class="music-info">
+                <img src="https://cdnimg.gamekee.com/wiki2.0/images/w_1920/h_1080/829/287349/2023/4/9/338318.jpg"
+                    alt="专辑图片">
+                <div class="info-bar">
+                    <div class="info">
+                        <span class="title"><strong>{{ title }}</strong></span>
+                        <span class="author">{{ author }}</span>
+                    </div>
+                    <div class="volume-bar">
+                        <button @click="volume_toggle"><font-awesome-icon
+                                :icon="isXmark ? ['fas', 'volume-xmark'] : ['fas', 'volume-high']" /></button>
+                        <v-slider v-model="volume" color="white" hide-details="true" @update:model-value="setVolume"
+                            :thumb-size="10"></v-slider>
+                    </div>
+                </div>
+                <div class="progress-bar">
+                    <strong>{{ currentTime1 }}</strong>
+                    <v-slider v-model="progress" color="white" hide-details="true" :thumb-size="0"
+                        @update:model-value="seekTo"></v-slider>
+                    <strong>{{ totalTime }}</strong>
+                </div>
+                <div class="switch">
+                    <button @click="mode_toggle" class="modeBtn"><font-awesome-icon :icon="['fas', 'shuffle']" /></button>
+                    <button @click="prev" class="sw"><font-awesome-icon :icon="['fas', 'step-backward']" /></button>
+                    <button @click="toggle" class="play"><font-awesome-icon
+                            :icon="isPlaying ? ['fas', 'pause'] : ['fas', 'play']" /></button>
+                    <button @click="next" class="sw"><font-awesome-icon :icon="['fas', 'step-forward']" /></button>
+                    <button @click="like"><font-awesome-icon :icon="['fas', 'heart']" /></button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlay, faPause, faStepForward, faStepBackward, faAngleLeft, faVolumeXmark, faVolumeHigh, faShuffle, faUpRightAndDownLeftFromCenter, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faPause, faStepForward, faStepBackward, faAngleLeft, faVolumeXmark, faVolumeHigh, faShuffle, faUpRightAndDownLeftFromCenter, faChevronUp, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faPlay, faPause, faStepForward, faStepBackward, faAngleLeft, faVolumeXmark, faVolumeHigh, faShuffle, faUpRightAndDownLeftFromCenter, faChevronUp)
+library.add(faPlay, faPause, faStepForward, faStepBackward, faAngleLeft, faVolumeXmark, faVolumeHigh, faShuffle, faUpRightAndDownLeftFromCenter, faChevronUp, faHeart)
 
 export default {
     data() {
         return {
-            isPlaying: false,
+            isPlaying: true,
             isClick: false,
-            volume: 50,
-            prevVolume: 50,
-            title: '歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名'
+            showSnackbar: false,
+            volume: 100,
+            prevVolume: 100,
+            progress: 0,
+            audioSrc: "https://dl.stream.qqmusic.qq.com/C400000GMGpe4IYOjH.m4a?guid=9318367020&vkey=A584029AA25229AD05B198F3D8FB8D80DC61FF71A0045ADF8C031B916562ADAEC13C3C6ACAB1BD663FC35D09F8C66F1184D1F769EB7B944A&uin=1294673511&fromtag=120032",
+            audioElement: null,
+            title: '歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名',
+            author: '歌手',
+            currentTime1: '00:00',
+            totalTime: '00:00'
         };
     },
     components: {
@@ -61,14 +102,27 @@ export default {
     mounted() {
         //写music-play用，记得删
         this.max();
+        this.audioElement = this.$refs.audioPlayer;
+        this.audioElement.autoplay = this.isPlaying;
+        this.audioElement.addEventListener('loadedmetadata', () => {
+            this.totalTime = this.formatTime(this.audioElement.duration);
+        });
+
     },
     methods: {
         prev() {
         },
         toggle() {
+            if (this.isPlaying) {
+                this.audioElement.pause();
+            } else {
+                this.audioElement.play();
+            }
             this.isPlaying = !this.isPlaying
         },
         next() {
+        },
+        like() {
         },
         volume_toggle() {
             if (this.volume !== 0) {
@@ -81,27 +135,33 @@ export default {
                 this.volume = this.prevVolume
             }
         },
+        setVolume() {
+            this.audioElement.volume = this.volume / 100;
+        },
         max() {
             const musicBar = document.querySelector('.music-bar');
             const musicPlay = document.querySelector('.music-play');
+            const musicPlayBg = document.querySelector('.music-play-bg');
             const returnButton = document.querySelector('.returnBtn');
 
-            if (!musicBar || !musicPlay || !returnButton) return;
-
+            if (!musicBar || !musicPlay || !musicPlayBg || !returnButton) return;
+            this.showSnackbar = true;
             function handleKeydown(event) {
                 if (event.keyCode === 27) {
-                    if (musicBar.style.transform === '' && musicPlay.style.transform === '' && returnButton.style.transform === '') {
+                    this.showSnackbar = false;
+                    if (musicBar.style.transform === '' && musicPlay.style.transform === '') {
                         document.removeEventListener('keydown', handleKeydown);
                     } else {
                         musicBar.style.transition = 'transform 0.3s';
                         musicPlay.style.transition = 'transform 0.3s';
+                        musicPlayBg.style.transition = 'transform 0.3s';
                         returnButton.style.transition = 'transform 0.3s';
                         setTimeout(() => {
                             musicBar.style.transform = '';
                             musicPlay.style.transform = '';
+                            musicPlayBg.style.transform = '';
                             returnButton.style.transform = '';
                         }, 0);
-
                     }
                 }
             }
@@ -109,25 +169,45 @@ export default {
             if (musicBar.style.transform === 'translateX(100vw)' && musicPlay.style.transform === 'translateX(-100vw)') {
                 musicBar.style.transition = '';
                 musicPlay.style.transition = '';
+                musicPlayBg.style.transition = '';
+                returnButton.style.transition = '';
                 setTimeout(() => {
                     musicBar.style.transform = '';
                     musicPlay.style.transform = '';
+                    musicPlayBg.style.transform = '';
                     returnButton.style.transform = '';
                 }, 300);
                 document.addEventListener('keydown', handleKeydown);
             } else {
                 musicBar.style.transition = 'transform 0.3s';
                 musicPlay.style.transition = 'transform 0.3s';
+                musicPlayBg.style.transition = 'transform 0.3s';
                 returnButton.style.transition = 'transform 0.3s';
                 setTimeout(() => {
                     musicBar.style.transform = 'translateX(100vw)';
                     musicPlay.style.transform = 'translateX(-100vw)';
+                    musicPlayBg.style.transform = 'translateX(-100vw)';
                     returnButton.style.transform = 'translateX(-10vw)';
                 }, 0);
                 document.addEventListener('keydown', handleKeydown);
             }
+        },
+        updateProgress() {
+            const currentTime = this.audioElement.currentTime;
+            const duration = this.audioElement.duration;
+            this.progress = (currentTime / duration) * 100;
+            this.currentTime1 = this.formatTime(this.audioElement.currentTime);
+        },
+        seekTo(value) {
+            const duration = this.audioElement.duration;
+            const seekTime = (value / 100) * duration;
+            this.audioElement.currentTime = seekTime;
+        },
+        formatTime(time) {
+            const minutes = Math.floor(time / 60);
+            const seconds = Math.floor(time % 60);
+            return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
         }
-
     },
     computed: {
         isXmark() {
@@ -190,7 +270,6 @@ svg {
     z-index: 1;
     background-image: url('../assets/imgs/music-background.jpg');
     background-size: cover;
-    backdrop-filter: blur(1000px);
     filter: blur(10px);
 }
 
@@ -236,6 +315,7 @@ svg {
     height: 5vw;
     border-radius: 10px;
     object-fit: cover;
+    pointer-events: none;
 }
 
 .music-bar .music-bar-info .music-info {
@@ -327,14 +407,128 @@ svg {
     height: 100%;
 }
 
-.music-play {
+.music-play-bg {
     position: absolute;
     top: 0;
     left: 100vw;
-
+    z-index: 2;
     width: 100vw;
     height: 100vh;
-    background-color: rgba(255, 255, 255, 0.4);
-    z-index: 5;
+
+    background-image: url('https://cdnimg.gamekee.com/wiki2.0/images/w_1920/h_1080/829/287349/2023/4/9/338318.jpg');
+    background-size: cover;
+    filter: blur(30px);
+}
+
+.music-play {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 100vw;
+    z-index: 3;
+    width: 100vw;
+    height: 100vh;
+
+    background-color: rgba(0, 0, 0, 0.3);
+}
+
+.music-play .music-info {
+    position: absolute;
+    top: 10vh;
+    left: 15vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 80vh;
+    width: 30vw;
+}
+
+.music-play .music-info img {
+    width: 30vw;
+    height: 30vw;
+    border-radius: 10px;
+    object-fit: cover;
+    pointer-events: none;
+    margin-bottom: 5%;
+}
+
+.music-play .music-info .info-bar {
+    height: 10%;
+    width: 100%;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.music-info .info-bar .info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    width: 60%;
+
+    color: white;
+}
+
+.music-info .info-bar .title {
+    width: 100%;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: larger;
+}
+
+.volume-bar {
+    display: flex;
+    align-items: center;
+    width: 30%;
+    height: 100%;
+}
+
+.volume-bar button {
+    height: 35%;
+    margin-right: 5%;
+}
+
+.volume-bar button svg {
+    height: 100%;
+}
+
+.music-info .progress-bar {
+    display: flex;
+    align-items: center;
+    color: white;
+    height: 5%;
+    width: 100%;
+    margin-block: 5%;
+}
+
+.music-info .switch {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    height: 10%;
+    width: 70%;
+}
+
+.music-info .switch button {
+    height: 30%;
+    width: 40%;
+}
+
+.music-info .switch .play {
+    height: 55%;
+}
+
+.music-info .switch .sw {
+    height: 40%;
+}
+
+.music-info .switch button svg {
+    height: 100%;
 }
 </style>
