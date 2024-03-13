04601b949c1955f4afeb9ff761c09be6e8b4d579
Wentian <1817429437@qq.com>
2024-03-13 09:37:53 +0800

    add upload api to the backend

--- BackEnd/apis.py
+++ BackEnd/apis.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request, make_response
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import (
     JWTManager,
@@ -15,6 +17,7 @@ from models import User, Anime
 from settings import db
 import math
 from datetime import timedelta, datetime
+import os 
 
 
 app = Flask(__name__)
@@ -91,6 +94,32 @@ class SignupApi(Resource):
         db.session.add(user)
         db.session.commit()
         return {"message": "Signup success"}, 201
+    
+# 上传文件到data目录
+class UploadApi(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('file', type=FileStorage, location='files', required=True, help="File is required")
+        parser.add_argument('resumableChunkNumber', type=int, required=True, help="Chunk number is required")
+        parser.add_argument('resumableTotalChunks', type=int, required=True, help="Total chunks is required")
+        args = parser.parse_args()
+
+        file = args['file']
+        chunk_number = args['resumableChunkNumber']
+        total_chunks = args['resumableTotalChunks']
+
+        filename = secure_filename(file.filename)
+        file.save(f'../Data/{filename}_part_{chunk_number}')
+
+        if chunk_number == total_chunks:
+            # All chunks have been uploaded, combine them
+            with open(f'../Data/{filename}', 'wb') as full_file:
+                for i in range(1, total_chunks + 1):
+                    with open(f'../Data/{filename}_part_{i}', 'rb') as part_file:
+                        full_file.write(part_file.read())
+                    os.remove(f'../Data/{filename}_part_{i}')  # Delete the chunk
+
+        return {"message": "Chunk successfully uploaded"}, 200
 
 
 class HomeApi(Resource):
