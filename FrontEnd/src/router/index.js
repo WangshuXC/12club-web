d942004f8b1313cc69d73660ed6abd8145b9767c
WangshuXC <1294673511@qq.com>
2024-03-26 16:46:43 +0800

    添加了amdin/op页面的token检测

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -74,9 +74,26 @@ const router = createRouter({
       component: () => import('../views/AdminView.vue'),
       meta: {
         showNavBar: false,
-        requiresAuth: true
+      },
+      beforeEnter: (to, from, next) => {
+        const access_token = getCookie('access_token_cookie');
+        const refresh_token = getCookie('refresh_token_cookie');
+
+        if (!access_token || !refresh_token) {
+          // 如果没有 access_token 或 refresh_token，重定向到 /admin 页面
+          next('/admin');
+        } else {
+          // 如果有 access_token 和 refresh_token，则允许进入 admin/op 页面
+          next();
+        }
       },
     }
   ]
 })
 export default router
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
